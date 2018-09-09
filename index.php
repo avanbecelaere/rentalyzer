b53994f26d5fe5160085526e12078be079881fe4
root <root@ip-172-31-60-186.ec2.internal>
2018-09-09 20:50:43 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+</head>
+<body>
+<?php
+echo "<h1>Welcome to rentalyzer.com</h1>";
+?>
+<br>
+<a href="statsByAddress.php">Stats by Address</a>
+</body>
+</html>
---
+++ statsByAddress.php
@@ -0,0 +1,33 @@
+<?php
+include_once("../config");
+$zillow_id = $zid; //the zillow web service ID that you got from your email
+
+#$search = $_GET['address'];
+#$citystate = $_GET['citystate'];
+$search = "3227 Lockridge Ave";
+$citystate = "Kansas City, MO";
+$address = urlencode($search);
+$citystatezip = urlencode($citystate);
+
+$url = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=$zillow_id&address=$address&citystatezip=$citystatezip";
+
+$result = file_get_contents($url);
+$data = simplexml_load_string($result);
+
+echo "<h1><center>Results For</center></h1>";
+echo "<center>$search<br>";
+echo "$citystate</center>";
+echo "<br>";
+echo "zpid: ";
+echo $data->response->results->result[0]->zpid;
+echo "<br>";
+echo "link-homedetails: ";
+$homeDetailLink = $data->response->results->result[0]->links->homedetails;
+echo "<a href=$homeDetailLink target='_blank'>$homeDetailLink</a>";
+echo '<br>';
+
+//$code = $data->message->code;
+//echo 'message code: ',$code;
+//echo '<br>';
+
+?>
