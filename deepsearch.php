ae75e1d445b66a4545e915d9ecd51d051ba0b9cd
Andy Van Becelaere <avanbecelaere@dataserv.com>
2018-09-10 22:29:08 -0500

    .

--- deepsearch.php
+++ deepsearch.php
@@ -2,15 +2,27 @@
 include_once("../config");
 
 $prefixurl = "http://www.zillow.com/webservice/GetDeepSearchResults.htm";
-$addr = urlencode("3227 Lockridge Ave");
-$cit = urlencode("Kansas City");
-$stat = urlencode("MO");
+$address = ("3323 Cleveland Ave");
+$city = ("Kansas City");
+$state = ("MO");
+$addr = urlencode($address);
+$cit = urlencode($city);
+$stat = urlencode($state);
 $csz = $cit."%2C+".$stat;
 $query = $prefixurl."?zws-id=".$zwsid."&address=".$addr."&citystatezip=".$csz."&rentzestimate=true";
-$sxml = simplexml_load_file(trim($query));
-echo json_encode($sxml);
+$result = simplexml_load_file(trim($query));
+$zpid = $result->response->results->result[0]->zpid;
+//echo json_encode($result);
 
-$zestimate = money_format('%n',floatval($sxml->response->results->result->zestimate->amount));
+echo "<h1><center>Results For</center></h1>";
+echo "<center>$address<br>";
+echo "$city, $state";
+echo "<br>";
+echo "zpid: ";
+echo $zpid;
+echo "</center><br><br>";
+
+$zestimate = money_format('%n',floatval($result->response->results->result->zestimate->amount));
 
 echo "<br>";
 echo "query: ";
@@ -18,5 +30,20 @@ echo "<a href=$query target='_blank'>$query</a>";
 echo "<br>";
 echo "zestimate: ";
 echo $zestimate;
+echo "<br>";
+
+$prefixurldetails = "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm";
+$detailsquery = $prefixurldetails."?zws-id=".$zwsid."&zpid=".$zpid;
+$detailsresult = simplexml_load_file(trim($detailsquery));
+$code = $detailsresult->message->code;
+if ($code == 0){
+	echo json_encode($detailsresult);
+	echo "<br>";
+	echo "detailsquery: ";
+	echo "<a href=$detailsquery target='_blank'>$detailsquery</a>";
+}
+else
+	echo "No Updates ($code)";
+
 
 ?>
