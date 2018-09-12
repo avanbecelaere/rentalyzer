cc2d7fb0802e332f58cbf91c249c560363a361a1
Andy Van Becelaere <avanbecelaere@dataserv.com>
2018-09-11 21:29:54 -0500

    .

--- search.php
+++ search.php
@@ -1,6 +1,7 @@
 <?php
 include_once("../config");
 
+// Request Data
 $prefixurl = "http://www.zillow.com/webservice/GetSearchResults.htm";
 $address = ("3323 Cleveland Ave");
 $city = ("Kansas City");
@@ -12,8 +13,20 @@ $csz = $cit."%2C+".$stat;
 $query = $prefixurl."?zws-id=".$zwsid."&address=".$addr."&citystatezip=".$csz."&rentzestimate=true";
 $result = simplexml_load_file(trim($query));
 $zpid = $result->response->results->result[0]->zpid;
-//echo json_encode($result);
 
+// Response Data & Calculations
+$prefixurldetails = "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm";
+$detailsquery = $prefixurldetails."?zws-id=".$zwsid."&zpid=".$zpid;
+$detailsresult = simplexml_load_file(trim($detailsquery));
+$zestimate = money_format('%n',floatval($result->response->results->result->zestimate->amount));
+$code = $detailsresult->message->code;
+if ($code == 0){
+	$updates = "<a href=$detailsquery target='_blank'>$detailsquery</a>";
+}
+else
+	$updates = "No Updates ($code)";
+
+// Header Output
 echo "<h1><center>Search Results For</center></h1>";
 echo "<center>$address<br>";
 echo "$city, $state";
@@ -21,32 +34,22 @@ echo "<br>";
 echo "zpid: ";
 echo $zpid;
 echo "</center><br>";
-
-$zestimate = money_format('%n',floatval($result->response->results->result->zestimate->amount));
-
-$prefixurldetails = "http://www.zillow.com/webservice/GetUpdatedPropertyDetails.htm";
-$detailsquery = $prefixurldetails."?zws-id=".$zwsid."&zpid=".$zpid;
-$detailsresult = simplexml_load_file(trim($detailsquery));
-$code = $detailsresult->message->code;
-if ($code == 0){
-	$updates = "<a href=$detailsquery target='_blank'>$detailsquery</a>";
-}
-else
-	$updates = "No Updates ($code)";
 ?>
 
 <br>
 <br>
+
+<!-- Table Output -->
 <table border=1>
 	<thead>
 		<tr>
 			<th>Label</th>
-			<th>Value</th>
+			<th>Response</th>
 		</tr>
 	</thead>
 	<tbody>
 		<?php
-			//Display Table Data
+			// Display Table Data
 			echo "<tr><td>Query</td> \n";
 			echo "<td><a href=$query target='_blank'>$query</a></td></tr> \n";
 			echo "<tr><td>Zestimate</td> \n";
