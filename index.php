6204afec8d81dc249d593bd3e2f8d0f1c8935e9b
Andy Van Becelaere <avanbecelaere@dataserv.com>
2018-09-12 21:09:20 -0500

    .

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 	<head>
 	</head>
 	<body>
-		<h1>Welcome to rentalyzer.com</h1>
+		<h1><center>Welcome to rentalyzer.com</center></h1>
 		<br>
 		<a href="search.php">GetSearchResults</a>
 		<br>
 		<h3 class="panel-title">Submit Address or Zillow ID</h3>
 		<form id="defaultForm" method="post" class="form-horizontal" role="form">
-			<label>Street</label>
+			<center><label>Street</label>
 			<input type="text" class="form-control" placeholder="Street" name="street" value=""><br><br>
 			<label>City</label>
 			<input type="text" class="form-control" placeholder="City" name="city" value=""><br><br>
@@ -17,7 +17,7 @@
 			<input type="text" class="form-control" placeholder="State" name="state" value=""><br><br>
 			<label>Zillow ID</label>
 			<input type="text" class="form-control" placeholder="Zillow ID" name="zillowID" value=""><br><br>
-			<input type="submit" value="Submit">
+			<input type="submit" value="Submit"></center>
 		</form>
 	</body>
 </html>
