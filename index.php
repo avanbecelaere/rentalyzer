9b8f35c5fb55f440663d585738d4219aac0da073
Andy Van Becelaere <avanbecelaere@dataserv.com>
2018-09-12 21:16:00 -0500

    .

--- index.php
+++ index.php
@@ -8,15 +8,15 @@
 		<center><a href="search.php">GetSearchResults</a></center>
 		<br>
 		<center><h3 class="panel-title">Submit Address or Zillow ID</h3></center>
-		<form id="defaultForm" method="post" class="form-horizontal" role="form">
+		<form id="defaultForm" action="/search.php" role="form">
 			<center><label>Street</label>
-			<input type="text" class="form-control" placeholder="Street" name="street" value=""><br><br>
+			<input type="text" placeholder="Street" name="street" value=""><br><br>
 			<label>City</label>
-			<input type="text" class="form-control" placeholder="City" name="city" value=""><br><br>
+			<input type="text" placeholder="City" name="city" value=""><br><br>
 			<label>State</label>
-			<input type="text" class="form-control" placeholder="State" name="state" value=""><br><br>
+			<input type="text" placeholder="State" name="state" value=""><br><br>
 			<label>Zillow ID</label>
-			<input type="text" class="form-control" placeholder="Zillow ID" name="zillowID" value=""><br><br>
+			<input type="text" placeholder="Zillow ID" name="zillowID" value=""><br><br>
 			<input type="submit" value="Submit"></center>
 		</form>
 	</body>
