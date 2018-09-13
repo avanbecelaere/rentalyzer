55af20be6d9f51d643668e750d750a3803515bc8
Andy Van Becelaere <avanbecelaere@dataserv.com>
2018-09-12 21:08:23 -0500

    .

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 		<h3 class="panel-title">Submit Address or Zillow ID</h3>
 		<form id="defaultForm" method="post" class="form-horizontal" role="form">
 			<label>Street</label>
-			<input type="text" class="form-control" placeholder="Street" name="street" value=""><br>
+			<input type="text" class="form-control" placeholder="Street" name="street" value=""><br><br>
 			<label>City</label>
-			<input type="text" class="form-control" placeholder="City" name="city" value="">
+			<input type="text" class="form-control" placeholder="City" name="city" value=""><br><br>
 			<label>State</label>
-			<input type="text" class="form-control" placeholder="State" name="state" value="">
+			<input type="text" class="form-control" placeholder="State" name="state" value=""><br><br>
 			<label>Zillow ID</label>
-			<input type="text" class="form-control" placeholder="Zillow ID" name="zillowID" value="">
+			<input type="text" class="form-control" placeholder="Zillow ID" name="zillowID" value=""><br><br>
 			<input type="submit" value="Submit">
 		</form>
 	</body>
